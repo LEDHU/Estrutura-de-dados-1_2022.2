17ac30eab0d4e6a8afa927ba826e6cd1614d90d8
Eduardo Peixoto <luis.00000845392@unicap.br>
2022-10-08 00:26:40 -0300

    paciente

--- Java/Main/br/unicap/luis_00000845392/p3/projeto/HealthSaude/Paciente.java
+++ Java/Main/br/unicap/luis_00000845392/p3/projeto/HealthSaude/Paciente.java
@@ -1,4 +1,7 @@
 package Java.Main.br.unicap.luis_00000845392.p3.projeto.HealthSaude;
 
 public class Paciente {
+    public static void main(String[] args) {
+        System.out.println("assasa");
+    }
 }
